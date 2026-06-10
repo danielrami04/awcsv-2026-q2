1f8560d34f0830fdc0c1dd751c9b0d57bff06bc6
danielrami04 <160197832+danielrami04@users.noreply.github.com>
2026-06-09 20:26:53 -0600

    act2

--- martes/js/practica5.js
+++ martes/js/practica5.js
@@ -1,31 +1,45 @@
-// Cambiar el título
-function modificarTitulo() {
-    document.getElementById("titulo").innerHTML =
-        "Título Modificado con JavaScript";
-}
+window.onload = function () {
 
-// Cambiar una imagen por otra
-function cambiarImagen() {
-    document.getElementById("imagen").src =
-        "https://picsum.photos/400";
-}
+    let btnTitulo = document.getElementById("btnTitulo");
+    let btnImagen = document.getElementById("btnImagen");
+    let btnTamanno = document.getElementById("btnTamanno");
+    let btnColor = document.getElementById("btnColor");
 
-// Cambiar tamaño de la imagen con botón
-function cambiarTamannoImagen() {
-    document.getElementById("imagen").style.width = "400px";
-}
+    let titulo = document.getElementById("titulo");
+    let imagen = document.getElementById("imagen");
+    let texto1 = document.getElementById("texto1");
+    let texto2 = document.getElementById("texto2");
 
-// Cambiar color de los textos con botón
-function cambiarColorTextos() {
-    document.getElementById("texto1").style.color = "red";
-    document.getElementById("texto2").style.color = "blue";
-}
+    // Cambiar título
+    btnTitulo.addEventListener("click", function () {
+        titulo.innerHTML = "Título Modificado con JavaScript";
+    });
 
-// Evento mouseenter para cambiar tamaño del texto
-document.getElementById("texto1").addEventListener("mouseenter", function () {
-    this.style.fontSize = "35px";
-});
+    // Cambiar imagen
+    btnImagen.addEventListener("click", function () {
+        imagen.src = "https://picsum.photos/400";
+    });
 
-document.getElementById("texto2").addEventListener("mouseenter", function () {
-    this.style.fontSize = "35px";
-});
+    // Cambiar tamaño de imagen
+    btnTamanno.addEventListener("click", function () {
+        imagen.style.width = "400px";
+    });
+
+    // Cambiar colores de los textos
+    btnColor.addEventListener("click", function () {
+        texto1.style.color = "red";
+        texto2.style.color = "blue";
+    });
+
+    // Mouse Enter sobre texto 1
+    texto1.addEventListener("mouseenter", function () {
+        texto1.style.fontSize = "35px";
+    });
+
+    // Mouse Enter sobre texto 2
+    texto2.addEventListener("mouseenter", function () {
+        texto2.style.fontSize = "35px";
+    });
+
+    console.log("Práctica 5 cargada correctamente");
+};
